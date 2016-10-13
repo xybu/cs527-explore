c3c702cd94fddb74ca3981a6b7ced608eafccfa3
Xiangyu Bu <xybu92@live.com>
2016-10-13 16:49:18 -0400

    Update suricata_docker.sh

--- scripts/suricata_docker.sh
+++ scripts/suricata_docker.sh
@@ -2,4 +2,4 @@
 
 cd suricata_docker
 sudo docker build --force-rm --tag xybu:suricata_afl --build-arg NPROC=`nproc` .
-docker create -it -w /root --name suricata_afl xybu:suricata_afl
+docker create -it -w /root --name suricata_afl xybu:suricata_afl --net host -v /home/xb:/host
